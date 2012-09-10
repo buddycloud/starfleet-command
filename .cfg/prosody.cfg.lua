c56a0cfa5e2653c977d16969d96d11dc5afe94bd
Thomas Jost <schnouki@schnouki.net>
2012-09-10 13:28:12 +0200

    Let Prosody use PLAIN auth with unencrypted connections
    Needed by the HTTP API…

--- .cfg/prosody.cfg.lua
+++ .cfg/prosody.cfg.lua
@@ -18,6 +18,7 @@ log     = {
 bosh_ports = {{ interface = "127.0.0.1"; port = 5280; }}
 consider_bosh_secure = true
 cross_domain_bosh = true
+allow_unencrypted_plain_auth = true
 
 -- enterprise.sf
 VirtualHost "enterprise.sf"
